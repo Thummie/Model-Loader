5a5b7a90dcba152100f7e1b5c41f936ea12f40b0
Thommie <53064581+Thummie@users.noreply.github.com>
2022-06-30 01:29:48 +0200

    Add files via upload

---
+++ cl_main.lua
@@ -0,0 +1,38 @@
+-- Request Model hash and load model
+function RequestModelHash(Model)
+    RequestModel(Model)
+      while not HasModelLoaded(Model) do
+        Wait(1)
+    end
+end
+
+exports('RequestModelHash', RequestModelHash)
+
+-- Request animation dictionary and load animation
+function RequestAnimationDict(AnimDict)
+    RequestAnimDict(AnimDict)
+    while not HasAnimDictLoaded(AnimDict) do
+        Citizen.Wait(1)
+    end
+end
+
+exports('RequestAnimDict', RequestAnimationDict)
+
+-- Request Animation Set and load animation set
+function RequestAnimSetEvent(AnimSet)
+    RequestAnimSet(AnimSet)
+    while not HasAnimSetLoaded(AnimSet) do
+        Citizen.Wait(0)
+    end
+end
+
+exports('RequestAnimSetEvent', RequestAnimSetEvent)
+
+-- Reset Animation Set
+function ResetAnimSet()
+    ResetPedMovementClipset(PlayerPedId())
+    ResetPedWeaponMovementClipset(PlayerPedId())
+    ResetPedStrafeClipset(PlayerPedId())
+end
+
+exports('ResetAnimSet', ResetAnimSet)
---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+version 'adamant'
+game 'gta5'
+
+description 'A small resource to load animation, models & animation set.'
+author 'Thommie'
+
+client_script 'cl_main.lua'
