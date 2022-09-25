d9144b8d30469956e883ab56c0ab4204b8eff910
Thommie <53064581+Thummie@users.noreply.github.com>
2022-09-25 18:22:32 +0200

    Load Particles added

--- cl_main.lua
+++ cl_main.lua
@@ -1,5 +1,5 @@
 -- Request Model hash and load model
-function RequestModelHash(Model)
+local function RequestModelHash(Model)
     RequestModel(Model)
       while not HasModelLoaded(Model) do
         Wait(1)
@@ -9,7 +9,7 @@ end
 exports('RequestModelHash', RequestModelHash)
 
 -- Request animation dictionary and load animation
-function RequestAnimationDict(AnimDict)
+local function RequestAnimationDict(AnimDict)
     RequestAnimDict(AnimDict)
     while not HasAnimDictLoaded(AnimDict) do
         Wait(1)
@@ -19,7 +19,7 @@ end
 exports('RequestAnimDict', RequestAnimationDict)
 
 -- Request Animation Set and load animation set
-function RequestAnimSetEvent(AnimSet)
+local function RequestAnimSetEvent(AnimSet)
     RequestAnimSet(AnimSet)
     while not HasAnimSetLoaded(AnimSet) do
         Wait(0)
@@ -29,10 +29,23 @@ end
 exports('RequestAnimSetEvent', RequestAnimSetEvent)
 
 -- Reset Animation Set
-function ResetAnimSet()
+local function ResetAnimSet()
     ResetPedMovementClipset(PlayerPedId())
     ResetPedWeaponMovementClipset(PlayerPedId())
     ResetPedStrafeClipset(PlayerPedId())
 end
 
 exports('ResetAnimSet', ResetAnimSet)
+
+-- Load particle
+local function LoadParticle(particle)
+    if not HasNamedPtfxAssetLoaded(particle) then
+        RequestNamedPtfxAsset(particle)
+    end
+    while not HasNamedPtfxAssetLoaded(particle) do
+        Wait(0)
+    end
+    SetPtfxAssetNextCall(partcle)
+end
+
+exports('LoadParticle', LoadParticle)
