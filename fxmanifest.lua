144267fedced9fdcebbb4488b74a80a7be79ebf0
Thommie <53064581+Thummie@users.noreply.github.com>
2023-09-03 15:19:31 +0200

    Update fxmanifest.lua
    Updated the fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'adamant'
 game 'gta5'
 
-description 'A small resource to load animation, models & animation set.'
+description 'A small resource to load animation, models, particles & animation set.'
 author 'Thommie'
-version '1.0.0'
+version '1.1.0'
 
 client_script 'cl_main.lua'
